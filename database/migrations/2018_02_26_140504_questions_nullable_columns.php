409b97ce2670a3092503c42493775cb33800cd98
Fadz <fadzilah.whyte@aol.co.uk>
2018-02-26 15:17:33 +0100

    Added doctrine/dbal and nullable columns

---
+++ database/migrations/2018_02_26_140504_questions_nullable_columns.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class QuestionsNullableColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->string('title')->nullable()->change();//make the column nullable
+            $table->text('text')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            //
+        });
+    }
+}
